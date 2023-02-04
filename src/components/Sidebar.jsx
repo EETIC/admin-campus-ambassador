f0132d5314ae72a5913b1e29aa88387baa56d2a6
jaidan22 <jaidansleeba22@gmail.com>
2023-02-04 21:55:13 +0530

    sidebar show

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -10,7 +10,7 @@ import ListItemText from "@mui/material/ListItemText";
 
 const Sidebar = () => {
   const [sidebar, setSidebar] = useState(true);
-  const [state, setState] = useState(true);
+  const [state, setState] = useState(false);
   // const
 
   // if screen width is less than 900px, sidebar will be hidden
@@ -95,39 +95,6 @@ const Sidebar = () => {
           </List>
         </Box>
       </Drawer>
-      {/* <div
-        className={`sidebar w-72 bg-blue-700 flex flex-col min-h-screen h-full ${
-          sidebar ? "" : "max-lg:hidden"
-        }`}
-      >
-        <div className="sidebar__header text-2xl p-2 text-slate-100 font-bold justify-center items-center">
-          <h1>EETI</h1>
-          <h3>CA Management</h3>
-        </div>
-        <div className="sidebar__links flex flex-col mt-6">
-          <a
-            href="/"
-            className="p-3 text-xl text-slate-100 font-bold hover:bg-blue-400"
-          >
-            Dashboard
-          </a>
-          <a
-            href="/list"
-            className="p-3 text-xl text-slate-100 font-bold hover:bg-blue-400"
-          >
-            View CA
-          </a>
-          <a
-            href="/profile"
-            className="p-3 text-xl text-slate-100 font-bold hover:bg-blue-400"
-          >
-            Profile
-          </a>
-        </div>
-      </div>
-      <div className="m-1 p-2 text-2xl cursor-pointer rounded-3xl hover:shadow-blue-300 hover:shadow-sm h-10">
-        <GiHamburgerMenu onClick={showSidebar} />
-      </div> */}
     </div>
   );
 };
